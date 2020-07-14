f7a0b5e55ff7f8cd913c04117e72d7928f91c660
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2020-07-14 08:02:34 +0530

    upload sky.py

---
+++ 1.py
@@ -0,0 +1,102 @@
+#python 2.7.15
+import time
+print "Hello,friend.."
+print "------welcome to sky----"
+print "log in or log up "
+user=raw_input("user: ")
+if user == "log in":
+    print "login"
+    username = raw_input("username: ") 
+    password = raw_input("password: ")
+    time.sleep(3)
+    print "log in successfull"
+elif user == "log up":   
+    print "log up"
+    username = raw_input("username: ")
+    password = raw_input("password: ")
+    mail = raw_input("e-mail id : ")
+    phone = raw_input("phone no: ")
+    time.sleep(3)
+    print "log up successfull"
+#from here program start   
+print "hello,my self Ro706 ..nice to meet u in this sky program u will learn many thing about earth ,space,and about ...so let start this :"
+print "how are u",username
+user = raw_input("user:")   
+if user == "fine":
+    print "nice"
+else:
+    print "why"
+    user=raw_input("user: ")
+    print "Oooo.."  
+print 
+print "nice too meet u ",username
+print "did u know who invented 0"
+print "yes / no"
+time.sleep(2)
+user=raw_input("user: ")
+if user == "yes":
+      user=raw_input("user: ")
+      if user == "aryabhata":
+          print "nice .."
+      else:
+          print "aryabhata"  
+        
+print "do u want to know about stars and space.."
+print "yes / no"
+user = raw_input("user: ")
+if user == "yes":
+    print " Stars are huge celestial bodies made mostly of hydrogen and helium that produce light and heat from the churning nuclear forges inside their cores. Aside from our sun, the dots of light we see in the sky are all light-years from Earth."
+else:
+    print "ok then ..do u want to share somthing about star..then u can.. "
+    print "yes/no"
+    user = raw_input("user: ")
+    if user == "yes":
+        print "ok.."
+        time.sleep(2)
+        user=raw_input("user: ")
+        time.sleep(1)
+        print "ok........... "
+        print "nice and thank u for the info"   
+    else:
+        print "ok"
+        time.sleep(1)
+  
+print "do u want a info about light year.."
+print "yes/no"
+user = raw_input("user: ")
+if user == "yes":
+    print "ok .."
+    time.sleep(2)
+    print "1ly = 9.4607 * 10^12" 
+else:    
+    print "so 1ly=what"
+    print "did u know this"
+    user = raw_input("user: ")
+    if user == "9.4607 * 10^12":
+        print "nice .."
+    else:
+        print "9.4607 * 10^12"
+        print "okay.."
+        time.sleep(2)
+print "do u want info about earth..."
+time.sleep(1)
+print "yes/no"
+user = raw_input("user: ")
+if user == "yes":
+    print "Facts about the Earth *The Earth's rotation is gradually slowing. ...*The Earth was once believed to be the centre of the universe. ...*Earth has a powerful magnetic field. ...*There is only one natural satellite of the planet Earth. ...*Earth is the only planet not named after a god. ...*The Earth is the densest planet in the Solar System."
+    time.sleep(3)
+    print "do u like it.."
+else:  
+    print "ok..."
+    
+print "ok bye..",username
+print "meet u soon.." 
+time.sleep(2)
+print "if u have any question than u can ask my friend google"
+print "---made by Ro706------"
+print "---github : https://github.com/Ro706 ---"
+print " Yesterday is history. Tomorrow is a mystery. ..."
+print ""
+print "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+
